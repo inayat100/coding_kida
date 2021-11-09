7df69da7d9ac1d3d591429033cac8475ef8bdb6a
inayat100 <inayat844127@gmail.com>
2021-11-09 21:00:41 +0530

    first commit

---
+++ bank/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import account,history,vrf,email_token,otp_save
+
+# Register your models here.
+@admin.register(account)
+class show(admin.ModelAdmin):
+    list_display = ['id','user','phone','addres','city','state','acn','amount','date_time']
+
+
+
+@admin.register(history)
+class histry(admin.ModelAdmin):
+    list_display = ['id','user','acn','crid','dev','action','date_time']
+
+@admin.register(vrf)
+class vrif(admin.ModelAdmin):
+    list_display = ['id','user','name','vf']
+
+
+@admin.register(email_token)
+class email_tokn(admin.ModelAdmin):
+    list_display = ['id','user','name','forget_token','created_at']
+
+@admin.register(otp_save)
+class email_tokn(admin.ModelAdmin):
+    list_display = ['id','user','name','otp','created_at']
---
+++ bank/thread.py
@@ -0,0 +1,117 @@
+import random
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
+from io import BytesIO
+import threading
+from .models import otp_save
+
+class class_email_otp(threading.Thread):
+    def __init__(self,email,o):
+        self.mail = email
+        self.otp = o
+        threading.Thread.__init__(self)
+    def run(self):
+        send_mail(
+            'otp for verifactions',
+            str(self.otp),
+            settings.EMAIL_HOST_USER,
+            [self.mail],
+            fail_silently=False
+        )
+        print("this done email...")
+
+class class_money_otp(threading.Thread):
+    def __init__(self, email, o):
+        self.mail = email
+        self.o = o
+        threading.Thread.__init__(self)
+    def run(self):
+
+        html_content = render_to_string("tem_email.html", {'otp': self.o})
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            "otp for verifactions",
+            text_content,
+            settings.EMAIL_HOST_USER,
+            [self.mail]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+
+class class_forget_password(threading.Thread):
+        def __init__(self, s_email,r_email,token,username):
+            self.mail = s_email
+            self.token = token
+            self.username = username
+            threading.Thread.__init__(self)
+
+        def run(self):
+            html_content = render_to_string("tem_email.html", {'token': self.token, 'name': self.username, 'reset': True})
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(
+                "otp for verifactions",
+                text_content,
+                settings.EMAIL_HOST_USER,
+                [self.mail]
+            )
+            email.attach_alternative(html_content, "text/html")
+            email.send()
+
+class class_send_money_sender(threading.Thread):
+    def __init__(self,email,fname,lname,money,account,time,status):
+        self.email = email
+        self.fname = fname
+        self.lname = lname
+        self.money = money
+        self.account = account
+        self.time = time
+        self.status = status
+        threading.Thread.__init__(self)
+
+    def run(self):
+        send = {'fname': self.fname, 'lname': self.lname, 'money': self.money, 'account': self.account, 'time': self.time, 'status': self.status,
+                'sender': True}
+        html_content = render_to_string("tem_email.html", send)
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            f'Sent Rs. {self.money} To {self.fname} {self.lname}',
+            text_content,
+            settings.EMAIL_HOST_USER,
+            [self.email]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+        print("this is send money classs if done now")
+
+
+class class_send_money_resever(threading.Thread):
+    def __init__(self,email,fname,lname,money,account,time,status):
+        self.email = email
+        self.fname = fname
+        self.lname = lname
+        self.money = money
+        self.account = account
+        self.time = time
+        self.status = status
+        threading.Thread.__init__(self)
+
+    def run(self):
+        send = {'fname': self.fname, 'lname': self.lname, 'money': self.money, 'account': self.account, 'time': self.time, 'status': self.status,
+                'resever': True}
+        html_content = render_to_string("tem_email.html", send)
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            f'Resever Rs. {self.money} From {self.fname} {self.lname}',
+            text_content,
+            settings.EMAIL_HOST_USER,
+            [self.email]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+        print("this is send money classs if done now")
