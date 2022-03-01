1ac36a9adba1e8634ed0c9ab8c4a715270e666f2
inayat100 <68042481+inayat100@users.noreply.github.com>
2022-03-01 15:48:50 +0530

    Update settings.py
    ok

--- dj13/settings.py
+++ dj13/settings.py
@@ -31,7 +31,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER ="inayat844127@gmail.com"
-EMAIL_HOST_PASSWORD = "Rasool@1234"
+EMAIL_HOST_PASSWORD = "***********"
 EMAIL_USE_TLS = True
 
 
